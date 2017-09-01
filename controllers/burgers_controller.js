808f42f60343ad37b6b8bbddbaa45f6d7171c6a1
mpleier <mpleier@gmail.com>
2017-09-01 13:36:41 -0700

    Initial.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,5 @@
+var burger = require(./models/burger.js');
+var express = require('express')
+
+
+4. Create the `router` for the app, and export the `router` at the end of your file.
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Beef Double Cheese Burger.');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon Burger.');
+INSERT INTO burgers (burger_name) VALUES ('Happy Meal Burger.');
