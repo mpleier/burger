9a5b22c828d95b5e824acc11828a0f58c6be0b51
mpleier <mpleier@gmail.com>
2017-09-04 11:00:51 -0700

    Heroku Config.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-iron-east-05.cleardb.net",
   port: 3306,
 
   // Your username
-  user: "root",
+  user: "b34a0edf013ed7",
 
   // Your password
-  password: "2648769853",
-  database: "burgers_db"
+  password: "a9e5fd7b",
+  database: "heroku_5ff183dcc65dcaa"
 });
 connection.connect(function(err) {
   if (err) {
