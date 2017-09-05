94dbdc12d10dd0df57de1404f0b49ebca22b3a7c
mpleier <mpleier@gmail.com>
2017-09-05 09:34:52 -0700

    Heroku DB.

--- config/connection.js
+++ config/connection.js
@@ -9,18 +9,19 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
+  // host: "localhost",
+  host: "us-cdbr-iron-east-05.cleardb.net",
   port: 3306,
 
   // Your username
-  // user: "b34a0edf013ed7",
-  user: "root",
+  user: "b34a0edf013ed7",
+  // user: "root",
 
   // Your password
-  // password: "a9e5fd7b",
-  // database: "heroku_5ff183dcc65dcaa"
-  password: "2648769853",
-  database: "burgers_db"
+  password: "a9e5fd7b",
+  database: "heroku_5ff183dcc65dcaa"
+  // password: "2648769853",
+  // database: "burgers_db"
 });
 connection.connect(function(err) {
   if (err) {
